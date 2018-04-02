a74d3278729d2ef6e3b25ad926fe434df490be79
Saugat Dhungel <saugat.dhungel@gmail.com>
2018-04-02 16:25:27 -0400

    factorial

--- recursion-problems.js
+++ recursion-problems.js
@@ -52,22 +52,58 @@
 
 // console.log(nthTriangularNumber([1, 3, 6, 10, 15, 21, 28, 36, 45]));
 
-const stringSplitter = (str) => {
+// const stringSplitter = (str) => {
 
-  if(!str.length){
-    return "";
-  }
-  if(str[0] === " "){
-    // return '.' +  stringSplitter(str.slice(1))
-    return `${'.'}${stringSplitter(str.slice(1))}`
-  }
-  // console.log(stringSplitter(str.split(" ")))
+//   if(!str.length){
+//     return "";
+//   }
+//   if(str[0] === " "){
+//     // return '.' +  stringSplitter(str.slice(1))
+//     return `${'.'}${stringSplitter(str.slice(1))}`
+//   }
+//   // console.log(stringSplitter(str.split(" ")))
 
-//  return str[0];
-//  return `${str[0]}/${stringSplitter(str.split(" "))}`;
-return `${str[0]}${stringSplitter(str.slice(1))}`
+// //  return str[0];
+// //  return `${str[0]}/${stringSplitter(str.split(" "))}`;
+// return `${str[0]}${stringSplitter(str.slice(1))}`
+// }
+
+// console.log(stringSplitter("hello hola bonjour"));
+
+'use strict';
+/**
+ * Write a recursive function that prints out the binary representation of a given number. For example, the program should take 3 as an input and print 11 as output, or 25 as an input and print 11001 as an output. Note that the binary representation of 0 should be 0.
+ */
+
+// const binaryRepresentation = num => {
+//   // the first answer to the first modulo is the last number in the binary sequence
+
+//   if (num === 0) return '';
+ 
+//   const binaryNumber = num % 2;
+//   // console.log(binaryNumber);
+
+//   const divisibleWholeNumber = Math.floor(num/2);
+//   // console.log(divisibleWholeNumber);
+    
+//   return `${binaryRepresentation(divisibleWholeNumber)} ${binaryNumber}`;
+    
+// };
+
+// console.log(binaryRepresentation(3));
+
+const factorial = (num) => {
+
+  if(num === 0){
+    return "1"
+  }
+
+  // const factNum = factorial(num - 1);
+
+  // // console.log(factNum)
+  // return num * factNum;
+return num * factorial(num - 1);
+
 }
 
-console.log(stringSplitter("hello hola bonjour"));
-
-//
+console.log(factorial(5));
